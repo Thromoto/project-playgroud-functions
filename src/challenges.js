b4fffe5c0648e899b850ef639bb7f21ae8b561e7
Thiago Omoto <thi.omoto@gmail.com>
2023-05-02 17:27:43 -0300

    req 10 e 11

--- src/challenges.js
+++ src/challenges.js
@@ -2,9 +2,8 @@
 function compareTrue(animal1, animal2) {
   if (animal1 === true && animal2 === true) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 
 // Desafio 2
@@ -21,7 +20,7 @@ function splitSentence(string) {
 function concatName(array) {
   let ultimaPalavra = array[array.length - 1];
   let primeiraPalavra = array[0];
-  return ultimaPalavra + ", " + primeiraPalavra;
+  return `${ultimaPalavra}, ${primeiraPalavra}`;
 }
 
 // Desafio 5
@@ -32,33 +31,32 @@ function footballPoints(wins, ties) {
 // Desafio 6
 function highestCount(array) {
   let maiorNum = array[0];
-  for (let index = 0; index < array.length; index += 1){
-    if(maiorNum < array[index]){
+  for (let index = 0; index < array.length; index += 1) {
+    if (maiorNum < array[index]) {
       maiorNum = array[index];
     }
   }
   let total = 0;
-  for (let index = 0; index < array.length; index += 1){
-    if (maiorNum === array[index]){
+  for (let index = 0; index < array.length; index += 1) {
+    if (maiorNum === array[index]) {
       total += 1;
     }
   }
   return total;
 }
-//Veja que neste caso a diferença é que você utiliza o array na primeira posição e logo após você verifica nas condicionais qual o maior valor e armazena na variável maiorNUm.
-//Em seguida é necessário contar quantas vezes este maior valor se repete, para isso você cria uma nova variável e verifica se a posição do array é igual ao maior valor e contabiliza.
+// Veja que neste caso a diferença é que você utiliza o array na primeira posição e logo após você verifica nas condicionais qual o maior valor e armazena na variável maiorNUm.
+// Em seguida é necessário contar quantas vezes este maior valor se repete, para isso você cria uma nova variável e verifica se a posição do array é igual ao maior valor e contabiliza.
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let fugir = 'os gatos trombam e o rato foge';
-  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)){
+  if (Math.abs(cat1 - mouse) < Math.abs(cat2 - mouse)) {
     fugir = 'cat1';
-  } else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)){
+  } else if (Math.abs(cat1 - mouse) > Math.abs(cat2 - mouse)) {
     fugir = 'cat2';
   }
   return fugir;
 }
-
 
 // Desafio 8
 function fizzBuzz(listaNumeros) {
@@ -75,14 +73,13 @@ function fizzBuzz(listaNumeros) {
     }
   }
   return resposta;
-
 }
 
 // Desafio 9
 function encode(frase) {
   let novaFrase = [];
-  for (let index = 0; index < frase.length; index += 1){
-    if (frase[index] === 'a'){
+  for (let index = 0; index < frase.length; index += 1) {
+    if (frase[index] === 'a') {
       novaFrase.push('1');
     } else if (frase[index] === 'e') {
       novaFrase.push('2');
@@ -101,8 +98,8 @@ function encode(frase) {
 
 function decode(frase) {
   let novaFrase = [];
-  for (let index = 0; index < frase.length; index += 1){
-    if (frase[index] === '1'){
+  for (let index = 0; index < frase.length; index += 1) {
+    if (frase[index] === '1') {
       novaFrase.push('a');
     } else if (frase[index] === '2') {
       novaFrase.push('e');
@@ -120,8 +117,20 @@ function decode(frase) {
 }
 
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(tech, nome) {
+  tech.sort();
+  if (tech.length === 0) {
+    return 'Vazio!';
+  }
+  let result = [];
+  for (let index of tech) {
+    const obj = {
+      tech: index,
+      name: nome,
+    };
+    result.push(obj);
+  }
+  return result;
 }
 
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,26 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(numeros) {
+  if (numeros.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  const contagem = {};
+
+  for (const num of numeros) {
+    if (num < 0 || num > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+    contagem[num] = (contagem[num] || 0) + 1;
+    if (contagem[num] >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+
+  const ddd = numeros.slice(0, 2).join('');
+  const primeiraParte = numeros.slice(2, 7).join('');
+  const segundaParte = numeros.slice(7).join('');
+
+  return `(${ddd}) ${primeiraParte}-${segundaParte}`;
 }
 
 // Desafio 12
